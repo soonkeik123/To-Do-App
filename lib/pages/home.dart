dd7de2d63c02b1676bd88360d82d483acded6939
soonkeik <soonkeik123@hotmail.com>
2024-05-27 03:08:42 +0800

    style: appbar & background modified

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -6,9 +7,35 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('ToDo App')),
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
       body: Center(
         child: Text('This is a home screen!'),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('assets/images/avatar.jpeg'),
+            ),
+          ),
+        ],
       ),
     );
   }
